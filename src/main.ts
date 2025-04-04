0ccb42e0e9a32fdad255ba7dcdd070ce336eae17
Francesco <you@example.com>
2025-04-04 17:32:28 +0200

    Add snack 3

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,107 @@ if (typeof dati === "string") {
   console.log(dati * 2);
 } else if (typeof dati === "boolean") {
   console.log(dati ? "Si" : "No");
+} else if (typeof dati === null) {
+  console.log("Il dato è vuoto");
 } else {
   console.log("tipo non supportato");
 }
+
+//! Snack 2
+
+/* Crea un type alias Dipendente che rappresenta un lavoratore con i seguenti dati:
+
+    nome → stringa
+    cognome → stringa
+    annoNascita → numero
+    sesso → Può essere solo "m" o "f".
+    anniDiServizio (array di numeri, es. [2014, 2015, 2017, 2018])
+
+🎯 BONUS
+
+Il type alias Dipendente, ha anche i seguenti dati:
+
+    emailAziendale → Email assegnata al dipendente (non si può modificare)
+    contratto → Specifica il tipo di contratto del dipendente, con valori limitati a “indeterminato”, “determinato” o “freelance”. */
+
+type Dipendente = {
+  nome: string;
+  cognome: string;
+  annoNascita: number;
+  sesso: "m" | "f";
+  anniDiServizio: number[];
+  readonly emailAziendale?: string;
+  contratto?: string;
+};
+
+const lavoratore: Dipendente = {
+  nome: "Marco",
+  cognome: "Rossi",
+  annoNascita: 1997,
+  sesso: "m",
+  anniDiServizio: [2015, 2016, 2017, 2018, 2019, 2020],
+  emailAziendale: "marco.rossi@emailAziendale.it",
+  contratto: "indeterminato",
+};
+
+//! Snack 3
+
+/* Estendiamo Dipendente per definire due ruoli specifici all'interno dell'azienda:
+
+    Developer
+        livelloEsperienza → Il livello di esperienza del developer (le scelte possibili sono solo “Junior”, “Mid” o “Senior”).
+        linguaggi → Un array contenente i linguaggi di programmazione utilizzati dal developer in azienda (opzionale, perché i neo assunti non hanno ancora dei linguaggi assegnati).
+        certificazioni → Un array di stringhe contenente certificazioni tecniche ottenute dal developer (può essere vuoto).
+    ProjectManager
+        teamSize → Il numero di persone nel team gestito dal Project Manager (può essere null se non ha ancora un team assegnato).
+        budgetGestito → Il totale del budget annuale gestito dal PM (opzionale).
+        stakeholderPrincipali → Un array di stringhe con i nomi dei principali stakeholder con cui il PM collabora (può essere vuoto).
+
+🎯 BONUS
+
+Definiamo un nuovo type alias Team, che rappresenta un gruppo di lavoro all'interno dell'azienda:
+
+    nome → Nome del team (stringa).
+    progettoAttuale → Nome del progetto su cui lavora il team (può essere null se il team è in attesa di assegnazione).
+    budget → Importo numerico del budget assegnato al team (sempre presente).
+    membri → Una tuple in cui il primo elemento è sempre un Project Manager, seguito da uno o più Developers (almeno un developer obbligatorio). */
+
+type Developer = Dipendente & {
+  livelloEsperienza: "Junior" | "Mid" | "Senior";
+  linguaggi?: string[];
+  certificazioni: string[];
+};
+
+type ProjectManager = Dipendente & {
+  teamSize: number | null;
+  budgetGestito?: number;
+  stakeholderPrincipali: string[];
+};
+
+const developer: Developer = {
+  nome: "Federico",
+  cognome: "Rossi",
+  annoNascita: 1988,
+  sesso: "m",
+  anniDiServizio: [2020, 2021, 2022, 2023, 2024, 2025],
+  emailAziendale: "federico.rossi@mailAziendale.it",
+  contratto: "freelance",
+  livelloEsperienza: "Senior",
+  linguaggi: ["Javascript", "Typescript", "Java", "Python"],
+  certificazioni: [],
+};
+
+const projectManager: ProjectManager = {
+  nome: "Michele",
+  cognome: "Rossi",
+  annoNascita: 1990,
+  sesso: "m",
+  anniDiServizio: [2018, 2019, 2020, 2021, 2022, 2023],
+  emailAziendale: "michele.rossi@mailAziendale.it",
+  contratto: "Indeterminato",
+  teamSize: 6,
+  budgetGestito: 15000,
+  stakeholderPrincipali: [],
+};
+
+console.log(lavoratore, developer, projectManager);
